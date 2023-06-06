085329b08f100159cc65b3d12f21682cd7ee6c3a
Cosmina <cosmi310599@gmail.com>
2023-06-06 22:43:17 +0200

    Update bot.py

--- pyvideo_bot/bot.py
+++ pyvideo_bot/bot.py
@@ -1,28 +1,61 @@
 import os
 import telebot
-from concurrent.futures import ThreadPoolExecutor
 from message_handlers import start_handler, help_handler, info_handler, default_handler
+from concurrent.futures import ThreadPoolExecutor
 
+# Retrieving the bot token from environment variables
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+# Creating a telebot instance
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# Creating a ThreadPoolExecutor instance for handling video download tasks
 executor = ThreadPoolExecutor(max_workers=5)
+
+# Download queue for each chat/user
+download_queues = {}
+
+# Menu for commands
+menu = telebot.types.ReplyKeyboardMarkup()
+menu.row("/start \U000025B6", "/info \U00002139", "/help \U00002754")
+
 
 @bot.message_handler(commands=['start', 'hello', 'Start', 'Hello'])
 def start(message):
+    """
+    Handle /start command.
+    """
     start_handler(bot, message)
+
 
 @bot.message_handler(commands=['help','Help'])
 def help(message):
-    help_handler(bot, message)
+    """
+    Handle /help command.
+    """
+    help_handler(bot, message, menu)
+
 
 @bot.message_handler(commands=['info','Info'])
 def info(message):
+    """
+    Handle /info command.
+    """
     info_handler(bot, message)
+
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    default_handler(bot, message, executor)
+    """
+    Handle all other messages that are not commands.
+    """
+    # Checking if there's already a download in progress for the chat
+    if message.chat.id in download_queues and not download_queues[message.chat.id].done():
+        bot.send_message(message.chat.id, 'There is already a download in progress. Please wait until it is completed.')
+        return
+
+    default_handler(bot, message)
+
 
 if __name__ == '__main__':
     bot.polling()
